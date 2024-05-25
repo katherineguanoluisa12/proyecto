b032a3d5753f8a2dab5e824072260d440117eb24
katherineguanoluisa12 <110473232+katherineguanoluisa12@users.noreply.github.com>
2024-05-25 16:13:56 -0500

    aa

--- main.js
+++ main.js
@@ -21,43 +21,6 @@ const provider = new GoogleAuthProvider(); // Proveedor de Google para autentica
 console.log("Conexión a Firebase establecida correctamente.");
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Registrar Usuario
-    const registrarForm = document.querySelector('#registrarse-form');
-    registrarForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const email = document.querySelector('#registrar-email').value;
-        const password = document.querySelector('#registrar-password').value;
-        console.log('Email:', email, 'Password:', password);
-        createUserWithEmailAndPassword(auth, email, password)
-            .then(userCredential => {
-                console.log('Usuario registrado:', userCredential.user);
-                registrarForm.reset();
-                $('#registrarModal').modal('hide');
-            })
-            .catch(error => {
-                console.error('Error al registrar el usuario:', error);
-                alert('Error al registrar el usuario. Por favor, verifica tus datos e intenta nuevamente.');
-            });
-    });
-
-    // Iniciar Sesión con Email y Password
-    const signinForm = document.querySelector('#iniciar-form');
-    signinForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const email = document.querySelector('#iniciar-email').value;
-        const password = document.querySelector('#iniciar-password').value;
-        console.log('Email:', email, 'Password:', password);
-        signInWithEmailAndPassword(auth, email, password)
-            .then(userCredential => {
-                console.log('Usuario inició sesión:', userCredential.user);
-                window.location.href = 'vehiculo.html'; // Redirigir a vehiculo.html después de iniciar sesión
-            })
-            .catch(error => {
-                console.error('Error al iniciar sesión:', error);
-                alert('Error al iniciar sesión. Por favor, verifica tus credenciales e intenta nuevamente.');
-            });
-    });
-
     // Iniciar Sesión con Google
     const googleSignInButton = document.querySelector('#google-signin');
     googleSignInButton.addEventListener('click', e => {
@@ -72,18 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 alert('Error al iniciar sesión con Google. Por favor, intenta nuevamente.');
             });
     });
-
-    // Cerrar Sesión
-    const logout = document.querySelector('#salir');
-    logout.addEventListener('click', (e) => {
-        e.preventDefault();
-        signOut(auth).then(() => {
-            console.log('Usuario cerró sesión');
-            window.location.href = 'index.html'; // Redirigir a la página principal después de cerrar sesión
-        }).catch((error) => {
-            console.error('Error al cerrar sesión:', error);
-        });
-    });
 });
+
 
 
