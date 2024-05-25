2a6a033d92a8283648089c6a143bcd16ee1b0bbf
katherineguanoluisa12 <110473232+katherineguanoluisa12@users.noreply.github.com>
2024-05-24 23:10:23 -0500

    regis

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const registrarForm = document.querySelector('#registrarse-form');
 registrarForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const registrarEmail = document.querySelector('#registrar-email').value;
-    const registrarPassword = document.querySelector('#registrar-password').value;
+    const Email = document.querySelector('#registrar-email').value;
+    const Password = document.querySelector('#registrar-password').value;
     console.log('Email:', registrarEmail, 'Password:', registrarPassword);
-});
+    Auth
+    .createUserWithEmailandPassword(Email,Password)
+    .then(userCredential=>{ 
+        console.long('registrar')
+
+    })
+})
