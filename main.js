808e80a0e1fcdcc166274d26ee9e5cc6f236db2b
katherineguanoluisa12 <110473232+katherineguanoluisa12@users.noreply.github.com>
2024-05-25 06:58:16 -0500

    listo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
 // Tu configuración de Firebase
@@ -20,6 +20,7 @@ const auth = getAuth(app); // Obtén la instancia de autenticación
 console.log("Conexión a Firebase establecida correctamente.");
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Registrar Usuario
     const registrarForm = document.querySelector('#registrarse-form');
     registrarForm.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -34,30 +35,38 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .catch(error => {
                 console.error('Error al registrar el usuario:', error);
+                alert('Error al registrar el usuario. Por favor, verifica tus datos e intenta nuevamente.');
             });
     });
-});
 
-document.addEventListener('DOMContentLoaded', () => {
+    // Iniciar Sesión
     const signinForm = document.querySelector('#iniciar-form');
     signinForm.addEventListener('submit', e => {
         e.preventDefault();
-
         const email = document.querySelector('#iniciar-email').value;
         const password = document.querySelector('#iniciar-password').value;
-
-        auth.signInWithEmailAndPassword(email, password)
+        console.log('Email:', email, 'Password:', password);
+        signInWithEmailAndPassword(auth, email, password)
             .then(userCredential => {
-                // Limpiar el formulario
+                console.log('Usuario inició sesión:', userCredential.user);
                 signinForm.reset();
-                // Cerrar el modal
                 $('#iniciarModal').modal('hide');
-                console.log('Inicio de sesión exitoso');
             })
             .catch(error => {
                 console.error('Error al iniciar sesión:', error);
                 alert('Error al iniciar sesión. Por favor, verifica tus credenciales e intenta nuevamente.');
             });
+    });
+
+    // Cerrar Sesión
+    const logout = document.querySelector('#salir');
+    logout.addEventListener('click', (e) => {
+        e.preventDefault();
+        signOut(auth).then(() => {
+            console.log('Usuario cerró sesión');
+        }).catch((error) => {
+            console.error('Error al cerrar sesión:', error);
+        });
     });
 });
 
