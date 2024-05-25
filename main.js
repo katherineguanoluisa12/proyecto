ca52672f9f198de041dd954aef17825d84141705
katherineguanoluisa12 <110473232+katherineguanoluisa12@users.noreply.github.com>
2024-05-24 23:20:48 -0500

    cambios

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+
+// Tu configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDvt5uALq0-Lp4XhG7Y8tjDZD7BOSxP_eI",
+    authDomain: "vehiculo-b415f.firebaseapp.com",
+    projectId: "vehiculo-b415f",
+    storageBucket: "vehiculo-b415f.appspot.com",
+    messagingSenderId: "61014179644",
+    appId: "1:61014179644:web:7d13477e77a85ac55ee839"
+};
+
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app); // Obtén la instancia de Firestore
 const auth = getAuth(app); // Obtén la instancia de autenticación
 
-const registrarForm = document.querySelector('#registrarse-form');
-registrarForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = document.querySelector('#registrar-email').value;
-    const password = document.querySelector('#registrar-password').value;
-    console.log('Email:', email, 'Password:', password);
-    createUserWithEmailAndPassword(auth, email, password)
-        .then(userCredential => {
-            console.log('Usuario registrado:', userCredential.user);
-        })
-        .catch(error => {
-            console.error('Error al registrar el usuario:', error);
-        });
+console.log("Conexión a Firebase establecida correctamente.");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const registrarForm = document.querySelector('#registrarse-form');
+    registrarForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const email = document.querySelector('#registrar-email').value;
+        const password = document.querySelector('#registrar-password').value;
+        console.log('Email:', email, 'Password:', password);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                console.log('Usuario registrado:', userCredential.user);
+            })
+            .catch(error => {
+                console.error('Error al registrar el usuario:', error);
+            });
+    });
 });
+
