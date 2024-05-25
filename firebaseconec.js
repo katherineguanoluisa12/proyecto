f419ae59b5c286b80de271d4a6fdc859d641728a
katherineguanoluisa12 <110473232+katherineguanoluisa12@users.noreply.github.com>
2024-05-24 20:09:19 -0500

    cambio de config

--- firebaseconec.js
+++ firebaseconec.js
@@ -1,7 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+// Ingresar en el head del index.html
+<script type="module" src="firebaseconect.js"></script>
 
-// Tu configuración de Firebase
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDvt5uALq0-Lp4XhG7Y8tjDZD7BOSxP_eI",
   authDomain: "vehiculo-b415f.firebaseapp.com",
@@ -11,8 +21,49 @@ const firebaseConfig = {
   appId: "1:61014179644:web:7d13477e77a85ac55ee839"
 };
 
-// Inicializar Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app); // Obtén la instancia de Firestore
 
-console.log("Conexión a Firebase establecida correctamente.");
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export class ManageAccount {
+  register(email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((_) => {
+        window.location.href = "login.html";
+        // Mostrar alerta de registro exitoso
+        alert("Registro exitoso. Serás redirigido a la página de inicio de sesión.");
+      })
+      .catch((error) => {
+        console.error(error.message);
+            // Mostrar alerta de error de registro
+            alert("Error al registrar: " + error.message);
+      });
+  }
+
+  authenticate(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((_) => {
+        window.location.href = "index.html";
+        // Mostrar alerta de inicio de sesión exitoso
+        alert("Has iniciado sesión correctamente. Serás redirigido a la página principal.");
+      })
+      .catch((error) => {
+        console.error(error.message);
+                // Mostrar alerta de error de inicio de sesión
+                alert("Error al iniciar sesión: " + error.message);
+      });
+  }
+
+  signOut() {
+    signOut(auth)
+      .then((_) => {
+        window.location.href = "index.html";
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+}
